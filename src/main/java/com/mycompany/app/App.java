fd84237cbc53f4bec3aee23ff29433b5e2f5d0af
Daniel Campos <daniel.campos@devfactory.com>
2016-04-27 15:03:17 -0300

    Testing

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -4,19 +4,21 @@ import java.util.Random;
 
 public class App {
     public String method1(int num) {
-        if (num > 7)
+        if (num > 7) {
             return "More than 7";
+        }
 
         return "Less than 7";
     }
 
-    public int doInt() {
+    public static int doInt() {
         return new Random().nextInt(8);
     }
 
     public static void main(String args[]) {
         System.out.println("****************************************");
         System.out.println("hello world!!!!");
+        System.out.println(doInt());
         System.out.println("****************************************");
     }
 }
