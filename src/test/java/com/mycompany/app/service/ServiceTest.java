60d5963fdd7c8497f9a1e177c5e477f5818ac043
Daniel Campos <daniel.campos@devfactory.com>
2016-04-28 13:25:41 -0300

    Testing

--- src/test/java/com/mycompany/app/service/ServiceTest.java
+++ src/test/java/com/mycompany/app/service/ServiceTest.java
@@ -3,6 +3,7 @@ package com.mycompany.app.service;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class ServiceTest {
 
@@ -10,5 +11,11 @@ public class ServiceTest {
     public void testServiceName() {
         Service s = new Service();
         assertEquals("Service", s.getServiceName());
+    }
+
+    @Test
+    public void testServiceId() {
+        Service s = new Service();
+        assertTrue(s.getServiceId() < 20);
     }
 }
