12b12203decb264dd98d32da661f75724767088a
Daniel Campos <daniel.campos@devfactory.com>
2016-04-28 13:26:08 -0300

    Testing

--- src/test/java/com/mycompany/app/service/ServiceTest.java
+++ src/test/java/com/mycompany/app/service/ServiceTest.java
@@ -1,10 +1,12 @@
 package com.mycompany.app.service;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+@Ignore
 public class ServiceTest {
 
     @Test
